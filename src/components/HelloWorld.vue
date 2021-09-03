56442f83c6c112900feb980e9ebb113fe01650bc
akkun08 <akihiroaika1112@gmail.com>
2021-09-03 14:10:38 +0900

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="alert alert-primary">
+    <h2>{{ title }}</h2>
+    <p>{{ message }}</p>
+    <hr />
+    <p class="h5">val:{{ val }}</p>
+    <div class="form-group text-left">
+      <label for="">*2:</label>
+      <input type="number" v-model="a" class="form-control" />
+    </div>
+    <div class="group-form text-left">
+      <label>^ 2:</label>
+      <input type="number" v-model="b" class="form-control" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    title: String,
+    num: {
+      type: Number,
+      default: 100,
+      validator: (value) => {
+        return value == parseInt(value) && value >= 0 && value <= 100;
+      },
+    },
+  },
+  data() {
+    return {
+      message: "バリデーション・チェック",
+    };
+  },
+};
+</script>
